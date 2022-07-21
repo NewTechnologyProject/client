c2c10927d2143b81c674de68f365404be1276cff
Tuan Khang <khangletuan098@gmail.com>
2022-07-21 20:37:36 +0700

    add

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const baseApi = "http://springserverapp-env.eba-yux4bcmw.ap-southeast-1.elasticbeanstalk.com/api/";
-// const URL = "https://realtimenode-env.eba-pnwam3yi.ap-southeast-1.elasticbeanstalk.com/";
-// const baseApi = 'http://localhost:5001/api/';
-const URL = "http://noderealtime-env.eba-dmm2gxfs.ap-southeast-1.elasticbeanstalk.com/";
+const baseApi = "https://springserverapp-env.eba-yux4bcmw.ap-southeast-1.elasticbeanstalk.com/api/";
+const URL = "https://noderealtime-env.eba-dmm2gxfs.ap-southeast-1.elasticbeanstalk.com/";
 const SOCKET_URL = URL
 
 export { URL };
